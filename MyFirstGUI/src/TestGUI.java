30fa8b1fca7d9e6bae7c575aff8c78de958cf318
daunelaugust <daunelaugustin@gmail.com>
2023-05-02 20:53:07 -0400

    pushes

--- MyFirstGUI/src/TestGUI.java
+++ MyFirstGUI/src/TestGUI.java
@@ -1,6 +1,16 @@
+import javax.swing.JLabel;
+import javax.swing.JFrame;
 
 public class TestGUI {
+	
 public static void main(String[] args) {
 	
+	
+	JLabel label = new JLabel("Yes I even code bro");
+	
+	JFrame frame = new JFrame();
+
+	
+	
 }
-}d
+}
